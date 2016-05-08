0111a9585dedbb68dad10c721f66d4d8668e0677
Thong Dong <thongdong7@gmail.com>
2016-05-09 05:56:12 +0700

    update require

---
+++ publish.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo Upload packages...
+python setup.py bdist_wheel --universal upload
--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from setuptools import setup
 
 setup(
     name='tb-ioc',
-    version='0.1.0',
+    version='0.1.2',
     packages=['tb_ioc'],
 
     author='Thong Dong',
     author_email='thongdong7@gmail.com',
     description='IOC (Inversion of control) for Python',
     url='https://github.com/thongdong7/tb-ioc',
+    install_requires=[
+        'pyyaml==3.11'
+    ]
 )
